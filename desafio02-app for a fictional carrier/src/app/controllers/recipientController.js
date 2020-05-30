c23b263246cc7fc438725bc5bdfffdaefd1d0f50
BRL4528 <blgc.sgo@hotmail.com>
2020-05-29 22:13:15 -0400

    add desafio02

---
+++ desafio02-app for a fictional carrier/src/app/controllers/recipientController.js
@@ -0,0 +1,79 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.string().required(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      cep: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.staus(400).json({ error: 'Validations Fails' });
+    }
+    const {
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    } = await Recipient.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.string().required(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      cep: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.staus(400).json({ error: 'Validations Fails' });
+    }
+    const {
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    } = await Recipient.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    });
+  }
+}
+export default new RecipientController();
---
+++ desafio02-app for a fictional carrier/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'c15ab45c8e1221d89f2c6a07b93ac2cb',
+  expiresIn: '7d',
+};
