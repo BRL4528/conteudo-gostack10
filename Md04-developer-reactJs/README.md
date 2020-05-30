b92e9481634ff3e98ad8f8d8bb3ec30bd32fb0b6
BRL4528 <blgc.sgo@hotmail.com>
2020-05-29 21:57:47 -0400

    add md04

---
+++ Md04-developer-reactJs/README.md
@@ -0,0 +1,2 @@
+# Bootcamp-GoStack-Md04
+Desenvolvimento ReactJs
---
+++ Md04-developer-reactJs/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import TechList from './components/TechList';
+
+function App() {
+    return <TechList/>
+}
+export default App;
